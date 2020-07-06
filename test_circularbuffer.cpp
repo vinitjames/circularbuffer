d027975bf4b36c0dd41671ba47e7c340e6749f8c
vinitjames <vinit.james@volkswagen.de>
2020-07-06 12:39:24 +0200

    fixed some bugs in front and back function and tested push back and pop fron tfunctionm

--- test_circularbuffer.cpp
+++ test_circularbuffer.cpp
@@ -21,13 +21,49 @@ int main(int argc, char *argv[])
 {   
 	
 	
-    CircularBuffer<test_struct> test{100};
+    CircularBuffer<int> test{5};
 	//std::cout<<"size of data "<<sizeof(data[1].bytes)<<"\n";
-	std::cout<<"size of buffer "<<test.size()<<"\n";
+	std::cout<<"length of buffer "<<test.size()<<"\n";
+	std::cout<<"size of buffer in bytes "<<test.buffer_size()<<"\n";
 	std::cout<<"Max capacity  of buffer "<<test.capacity()<<"\n";
 	std::cout<<"Checking is buffer empty function "<<test.empty()<<"\n";
+	std::cout<<"Pushing back  data in bufffer \n";
+	test.push_back(5);
+	test.push_back(2);
+	//test.push_back(3);
+	std::cout<<"Checking is buffer empty function "<<test.empty()<<"\n";
+	std::cout<<"Checking length of buffer after modification "<<test.size()<<"\n";
+	std::cout<<"Checking front function  "<<test.front()<<"\n";
+	std::cout<<"Checking front function  "<<test.front()<<"\n";
+	std::cout<<"Checking back function  "<<test.back()<<"\n";
+	test.back() = 10;
 	
-	std::cout<<"size of  test buffer "<<test.buffer_size()<<"\n";
+	std::cout<<"Checking pop function\n";
+	test.pop_front();
+	std::cout<<"Checking front function  "<<test.front()<<"\n";
+	std::cout<<"Checking back function  "<<test.back()<<"\n";
+	//std::cout<<"Checking []operator  "<<test[10]<<"\n";
+
+	CircularBuffer<std::string> test_stringbuf{10};
+	std::cout<<"Checking is buffer empty function "<<test_stringbuf.empty()<<"\n";
+	std::cout<<"Checking length of string buffer "<<test_stringbuf.size()<<"\n";
+	std::cout<<"size of buffer in bytes "<<test_stringbuf.buffer_size()<<"\n";
+	std::cout<<"Max capacity  of buffer "<<test_stringbuf.capacity()<<"\n";
+	
+	std::cout<<"Pushing back  data in bufffer \n";
+	test_stringbuf.push_back("this is the first string");	
+	test_stringbuf.push_back("this is the second string");
+	test_stringbuf.push_back("this is the third string"); 
+
+	std::cout<<"Checking is buffer empty function "<<test_stringbuf.empty()<<"\n";
+	std::cout<<"Checking length of buffer after modification "<<test_stringbuf.size()<<"\n";
+	std::cout<<"Checking front function  "<<test_stringbuf.front()<<"\n";
+	std::cout<<"Checking back function  "<<test_stringbuf.back()<<"\n";
+	test_stringbuf.pop_front();
+	test_stringbuf.pop_front();
+	std::cout<<"Checking length of buffer after modification "<<test_stringbuf.size()<<"\n";
+	std::cout<<"Checking front function  "<<test_stringbuf.front()<<"\n";
+	std::cout<<"Checking back function  "<<test_stringbuf.back()<<"\n";
 	//while(true){}
     return 0;
 }
